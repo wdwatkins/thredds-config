34c2d1b5332a65b3c6e4b6faa18dad7fd601c838
Megan Hines <mhines@usgs.gov>
2015-10-27 14:14:20 -0500

    Small tweaks to script and output

--- mnt_thredds_optimized/cooper/rewrite nccalls.R
+++ mnt_thredds_optimized/cooper/rewrite nccalls.R
@@ -2,20 +2,20 @@ library(whisker)
 library(stringr)
 
 vars <- readLines("./UpperDeschutes_variables.txt")
-vars <- str_replace(vars,"/","_")
 ncCalls<-list()
 for (i in 1:length(vars)) { 
   ncCalls<-paste0("nccopy -k 3 -d 1 -c time/1,UTM_Meters_North/30,UTM_Meters_East/30 -u http://cida-eros-netcdfdev.er.usgs.gov:8080/thredds/dodsC/thredds_workspace/cooper/cooper_meta.ncml?lon[0:1:780][0:1:384],UTM_Meters_North[0:1:780],lat[0:1:780][0:1:384],UTM_Meters_East[0:1:384],time[0:1:8394],", vars, " /mnt/cooper/data/UpperDeschutes/",vars,".nc")
 } 
 data<-list(ncCalls)
-cat(whisker.render(data), file="UpperDeschutes_nccalls.txt", append=FALSE)
-lapply(data, write, "test.txt", append=FALSE, ncolumns=1000)
+#cat(whisker.render(data), file="UpperDeschutes_nccalls.txt", append=FALSE)
+lapply(data, write, "UpperDeschutes_nccalls.txt", append=FALSE, ncolumns=1000)
 
 
 vars <- readLines("./McKenzie_variables.txt")
 ncCalls<-list()
 for (i in 1:length(vars)) { 
-  ncCalls<-paste0("nccopy -k 3 -d 1 -c time/1,UTM_Meters_North/30,UTM_Meters_East/30 -u http://cida-eros-netcdfdev.er.usgs.gov:8080/thredds/dodsC/thredds_workspace/cooper/cooper_meta.ncml?lon[0:1:780][0:1:384],UTM_Meters_North[0:1:780],lat[0:1:780][0:1:384],UTM_Meters_East[0:1:384],time[0:1:8394],", vars, " /mnt/cooper/data/McKenzie/",vars,"/")
+  ncCalls<-paste0("nccopy -k 3 -d 1 -c time/1,UTM_Meters_North/30,UTM_Meters_East/30 -u http://cida-eros-netcdfdev.er.usgs.gov:8080/thredds/dodsC/thredds_workspace/cooper/cooper_meta.ncml?lon[0:1:780][0:1:384],UTM_Meters_North[0:1:780],lat[0:1:780][0:1:384],UTM_Meters_East[0:1:384],time[0:1:8394],", vars, " /mnt/cooper/data/McKenzie/",vars,".nc")
 } 
 data<-list(ncCalls)
-cat(whisker.render(as.character(data)), file="McKenzie_nccalls.txt", append=FALSE)
+#cat(whisker.render(as.character(data)), file="McKenzie_nccalls.txt", append=FALSE)
+lapply(data, write, "McKenzie_nccalls.txt", append=FALSE, ncolumns=1000)
