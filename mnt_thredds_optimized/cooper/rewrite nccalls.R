1473ef2b67405836e1e9812954d6febc54258dfa
Megan Hines <mhines@usgs.gov>
2015-10-27 15:56:03 -0500

    Removing indices which are unnecessary and adding newlines

--- mnt_thredds_optimized/cooper/rewrite nccalls.R
+++ mnt_thredds_optimized/cooper/rewrite nccalls.R
@@ -4,7 +4,7 @@ library(stringr)
 vars <- readLines("./UpperDeschutes_variables.txt")
 ncCalls<-list()
 for (i in 1:length(vars)) { 
-  ncCalls<-paste0("nccopy -k 3 -d 1 -c time/1,UTM_Meters_North/30,UTM_Meters_East/30 -u http://cida-eros-netcdfdev.er.usgs.gov:8080/thredds/dodsC/thredds_workspace/cooper/cooper_union_deschutes.ncml?lon[0:1:780][0:1:384],UTM_Meters_North[0:1:780],lat[0:1:780][0:1:384],UTM_Meters_East[0:1:384],time[0:1:8394],", vars, " /mnt/thredds/cooper/data/UpperDeschutes/",vars,".nc")
+  ncCalls<-paste0("nccopy -k 3 -d 1 -c time/1,UTM_Meters_North/30,UTM_Meters_East/30 -u http://cida-eros-netcdfdev.er.usgs.gov:8080/thredds/dodsC/thredds_workspace/cooper/cooper_union_deschutes.ncml?lon,UTM_Meters_North,lat,UTM_Meters_East,time,", vars, " /mnt/thredds/cooper/data/UpperDeschutes/",vars,".nc\n")
 } 
 data<-list(ncCalls)
 #cat(whisker.render(data), file="UpperDeschutes_nccalls.txt", append=FALSE)
@@ -14,7 +14,7 @@ lapply(data, write, "UpperDeschutes_nccalls.txt", append=FALSE, ncolumns=1000)
 vars <- readLines("./McKenzie_variables.txt")
 ncCalls<-list()
 for (i in 1:length(vars)) { 
-  ncCalls<-paste0("nccopy -k 3 -d 1 -c time/1,UTM_Meters_North/30,UTM_Meters_East/30 -u http://cida-eros-netcdfdev.er.usgs.gov:8080/thredds/dodsC/thredds_workspace/cooper/cooper_union_mckenzie.ncml?lon[0:1:758][0:1:1120],UTM_Meters_North[0:1:758],lat[0:1:758][0:1:1120],UTM_Meters_East[0:1:1120],time[0:1:7664],", vars, " /mnt/thredds/cooper/data/McKenzie/",vars,".nc")
+  ncCalls<-paste0("nccopy -k 3 -d 1 -c time/1,UTM_Meters_North/30,UTM_Meters_East/30 -u http://cida-eros-netcdfdev.er.usgs.gov:8080/thredds/dodsC/thredds_workspace/cooper/cooper_union_mckenzie.ncml?lon,UTM_Meters_North,lat,UTM_Meters_East,time,", vars, " /mnt/thredds/cooper/data/McKenzie/",vars,".nc\n")
 } 
 data<-list(ncCalls)
 #cat(whisker.render(as.character(data)), file="McKenzie_nccalls.txt", append=FALSE)
