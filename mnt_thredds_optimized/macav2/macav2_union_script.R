bae6fb127fe688e8417cf990d360d83dd256f6f7
David Blodgett <dblodgett@usgs.gov>
2016-05-02 11:21:12 -0500

    macav2 aggregation script and ncml files

---
+++ mnt_thredds_optimized/macav2/macav2_union_script.R
@@ -0,0 +1,48 @@
+library(stringr)
+library(whisker)
+
+templatefun<-function(models,rcps,vars,files,long_vars,outname,searchstring) {
+  count<-0
+  joinList<-list()
+  for (i in 1:length(models)) { # First level loop over 20 models
+    for (j in 1:length(rcps)) { # Second level loop over 3 time periods
+      varList<-list()
+      for (l in 1:length(vars)) { # Third level loop over 10 variables
+        joinFileList<-list()
+        for(k in 1:length(files)) { # Fourth level loop over all files to ensure files exist.
+          if(grepl(pattern = paste0(vars[l], "_", models[i], "_r1i1p1_", rcps[j], ".*",searchstring,".*"), x = files[k])) {
+            joinFileList<-append(joinFileList,list(list(fileName=files[k])))
+            count<-count+1
+          }
+        }
+        if(length(joinFileList)>0) {
+          joinList<-append(joinList,list(list(joinFiles=joinFileList,orgName=long_vars[vars[l]], name=paste0(vars[l], "_", models[i], "_r1i1p1_", rcps[j]))))
+        }
+      }
+    }
+  }
+  data<-list(joins=joinList)
+  template<-readLines("./macav2_template.ncml")
+  cat(whisker.render(template, data), file=outname, append=FALSE)
+  return(paste('Put',count,'files into ncml file',outname))}
+
+files <- read.csv("./macav2_files.csv",stringsAsFactors = FALSE)$x
+vars <- readLines("./macav2_vars.txt")
+long_vars <- list(pr="precipitation",
+                  huss="specific_humidity",
+                  rhsmax="relative_humidity",
+                  rhsmin="relative_humidity",
+                  rsds="surface_downwelling_shortwave_flux_in_air",
+                  tasmax="air_temperature",
+                  tasmin="air_temperature",
+                  uas="eastward_wind",
+                  vas="northward_wind",
+                  was="wind_speed")
+models <- readLines("./macav2_models.txt")
+rcps<-c("rcp45","rcp85")
+templatefun(models,rcps,vars,files,long_vars,'macav2_future_daily.ncml',"daily")
+templatefun(models,rcps,vars,files,long_vars,'macav2_future_monthly.ncml',"monthly")
+rcps<-c("historical")
+templatefun(models,rcps,vars,files,long_vars,'macav2_historical_daily.ncml',"daily")
+templatefun(models,rcps,vars,files,long_vars,'macav2_historical_monthly.ncml',"monthly")
+
