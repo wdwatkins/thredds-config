969fecb70a0646201a71cd171fea3002182dc280
David Blodgett <dblodgett@usgs.gov>
2016-08-03 13:35:23 -0500

    ready to go to prod

---
+++ mnt_thredds_optimized/ssebopeta/nco.sh
@@ -0,0 +1,18 @@
+for f in *.tif; do gdal_translate -of netCDF -co "FORMAT=NC4" -a_nodata 9999 -ot int16 $f ${f%.*}.nc; done
+files_to_cat=""
+for f in *.nc; do files_to_cat="${files_to_cat} $f"; ncrename -h -O -v Band1,et $f; done
+ncecat -h -O -u time ${files_to_cat} y.modisSSEBopET.nc
+ncks -h -A ../times_annual.nc y.modisSSEBopET.nc 
+ncatted -h -O -a Metadata_Conventions,global,o,c,'Unidata Dataset Discovery v1.0' -a title,global,o,c,"Conterminous U.S. actual evapotranspiration data"  y.modisSSEBopET.nc 
+ncatted -h -O -a creator_name,global,o,c,"Stefanie Bohms" -a creator_email,global,o,c,"sbohms@usgs.gov" -a long_name,et,o,c,"Actual Evapotranspiration" -a units,et,o,c,mm y.modisSSEBopET.nc 
+ncatted -h -O -a history,global,d,, -a GDAL,global,d,, -a NCO,global,d,, -a GDAL_AREA_OR_POINT,global,d,, -a GDAL_DataType,global,d,, y.modisSSEBopET.nc 
+ 
+for f in *; do mv $f/* ./; done
+for f in *.tif; do gdal_translate -of netCDF -co "FORMAT=NC4" -a_nodata 9999 -ot int16 $f ${f%.*}.nc; done
+files_to_cat=""
+for f in *.nc; do files_to_cat="${files_to_cat} $f"; ncrename -h -O -v Band1,et $f; done
+ncecat -h -O -u time ${files_to_cat} m.modisSSEBopET.nc
+ncks -h -A ../times_monthly.nc m.modisSSEBopET.nc 
+ncatted -h -O -a Metadata_Conventions,global,o,c,'Unidata Dataset Discovery v1.0' -a title,global,o,c,"Conterminous U.S. actual evapotranspiration data"  m.modisSSEBopET.nc 
+ncatted -h -O -a creator_name,global,o,c,"Stefanie Bohms" -a creator_email,global,o,c,"sbohms@usgs.gov" -a long_name,et,o,c,"Actual Evapotranspiration" -a units,et,o,c,mm m.modisSSEBopET.nc 
+ncatted -h -O -a history,global,d,, -a GDAL,global,d,, -a NCO,global,d,, -a GDAL_AREA_OR_POINT,global,d,, -a GDAL_DataType,global,d,, m.modisSSEBopET.nc 
