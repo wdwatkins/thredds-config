4fc53e558dbac883d9f2a46e2161c10cd3155225
wdwatkins <wwatkins@usgs.gov>
2017-01-27 16:49:18 -0600

    union working

--- mnt_thredds_optimized/puerto_rico/puertoRic_union.R
+++ mnt_thredds_optimized/puerto_rico/puertoRic_union.R
@@ -3,7 +3,7 @@ library(whisker)
 domains <- c("d01","d02","d03")
 models <- c("CNRM", "CESM")
 tempRes <- c("monthly", "WRF_EXTRACT")
-template<-readLines("./loca_template_join.ncml")
+joinTemplate<-readLines("./loca_template_join.ncml")
 dir.create('ncml_joins')
 allFiles <- scan('allFiles_noAll.txt', what = "character")
 
@@ -19,17 +19,44 @@ for(d in domains) {
     #narrow to model
     for(m in models) {
       modelFiles <- tempResFiles[grepl(pattern = m, tempResFiles)]
-      
-      #now create join ncml
-      #create join names
-      joinName <- paste(d, t, m, sep = "_")
-      joinFiles <- paste0("../", modelFiles)
     
-      #render
-      cat(whisker.render(template), 
-          file=paste0("ncml_joins/",joinName,".ncml"), append=FALSE)  
+        #now create join ncml
+        #create join names
+        joinName <- paste(d, t, m, sep = "_")
+        joinFiles <- paste0("../", modelFiles)
+        
+        #render
+        cat(whisker.render(joinTemplate), 
+            file=paste0("ncml_joins/",joinName,".ncml"), append=FALSE)  
     }
   }
 }
 
-#create ncml of all joins 
+unionTemplate <- readLines('puertoRico_rename_template.ncml')
+#now do rename variables
+for(t in tempRes) {
+  if(t == "monthly") {
+    vars <- scan('monthlyVars.txt', what = "character")
+  }
+  if(t == "WRF_EXTRACT") {
+    vars <- scan('wrfEx_vars.txt', what = "character")
+  }
+  
+  #find matching joins
+  matchingJoins <- list.files("./ncml_joins", pattern = t)
+  pairs <- list()
+  joins <- list()
+  for(j in 1:length(matchingJoins)) {
+    for(v in 1:length(vars)) {
+      pairs[[v]] <- list(origName = vars[v], newName = paste(vars[v], matchingJoins[j], sep = "_"))
+    }
+    joins[[j]] <- list(joinName = matchingJoins[j], pairs = pairs)
+  }
+  
+  cat(whisker.render(template = unionTemplate, list(joins = joins)), file = paste0(t, ".ncml"))
+  
+  
+  
+}
+
+
