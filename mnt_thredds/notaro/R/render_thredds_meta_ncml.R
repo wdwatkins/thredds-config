4eb8a5af676e458277a0eb020cfe7bc21702d0a2
dataScienceUser <wwatkins@usgs.gov>
2018-06-25 13:23:32 -0500

    typos

--- mnt_thredds/notaro/R/render_thredds_meta_ncml.R
+++ mnt_thredds/notaro/R/render_thredds_meta_ncml.R
@@ -5,6 +5,6 @@ meta[['location']] <- '/mnt/morethredds/notaro_2018/join_union_CNRM_1980_1990.nc
 meta <- meta[!unlist(lapply(meta, is.null))]
 
 out <- whisker::whisker.render(template, meta)
-cat(out, file = "thredds_config/notaro_2018_CNRM_1980.cnml")
-file.copy(from = "thredds_config/notaro_2018_CNRM_1980.cnml", 
-          "~/Documents/R/thredds-config/content/thredds/metadata/notaro_2018/notaro_2018_CNRM_1980.cnml")
+cat(out, file = "thredds_config/notaro_2018_CNRM_1980.ncml")
+file.copy(from = "thredds_config/notaro_2018_CNRM_1980.ncml", 
+          "~/Documents/R/thredds-config/content/thredds/metadata/notaro_2018/notaro_2018_CNRM_1980.ncml")
