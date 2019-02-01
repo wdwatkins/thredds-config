1cff36545476a91c2db37c2a3d82b1adb2a65a0a
wdwatkins <wwatkins@usgs.gov>
2019-02-01 13:52:07 -0800

    check netcdf time var for duplicates

--- mnt_thredds/puerto_rico/remove_nhm_time_duplicates/check_time_duplicates.R
+++ mnt_thredds/puerto_rico/remove_nhm_time_duplicates/check_time_duplicates.R
@@ -2,11 +2,12 @@
 ###for GFDL -- CCSM4 is different
 library(ncdf4)
 library(tidyverse)
-nc <- nc_open('~/Downloads/pr_duplicate_dates/CLA_hrly.nc') #GFDL file
+nc <- nc_open('~/Downloads/pr_duplicate_dates/CLA_hrly_ccsm4.nc') 
 time <- ncvar_get(nc, "time")
 #check for duplicates with fromLast=TRUE
 time_df <- tibble(time_hrs = time, dateTime = as.POSIXct(time*3600, origin="1986-01-01 00:00", tz = "UTC"))
 duplicated_time_positions <- which(duplicated(time, fromLast = TRUE))
+dup_df <- slice(time_df, duplicated_time_positions)
 #need to format this into strings for NCO
 #e.g. ncks -d Time,0,433 -d Time,435,598 -d Time,600,1013 wrfout_1 wrfout_2
 #get start/end postions, then combine
@@ -14,5 +15,5 @@ duplicated_time_positions <- which(duplicated(time, fromLast = TRUE))
 starts <- c(1,duplicated_time_positions+1) - 1
 ends <- c(duplicated_time_positions-1, length(time)) - 1
 strings <- paste(paste("-d time", starts, ends, sep = ","), collapse = " ")
-write_lines(strings, path = "GFDL_NHM_time_multislabs.txt")
+write_lines(strings, path = "CCSM4_CLA_NHM_time_multislabs.txt")
 nc_close(nc)
