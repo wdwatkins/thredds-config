13e9dc98e5ede9bbfc663b640fa9ce5e41cddbd1
wdwatkins <wwatkins@usgs.gov>
2020-05-01 15:49:24 -0700

    switch to prod URLs

--- mnt_thredds/TTU_LSU/render_metdata.R
+++ mnt_thredds/TTU_LSU/render_metdata.R
@@ -45,7 +45,7 @@ for(ds in dataset_names) {
     start_time = meta$start_time,
     end_time = meta$end_time,
     note = meta$note,
-    thredds_url = file.path('https://cida-test.er.usgs.gov/thredds/dodsC/', dataset_url_path),
+    thredds_url = file.path('https://cida.usgs.gov/thredds/dodsC/', dataset_url_path),
     service_id = paste('OPeNDAP', i, sep = "_"))
   i <- i + 1
   meta[['attribute_description']] <- list(ddx_description = meta$attribute_description[[2]],
@@ -59,31 +59,3 @@ cat(catalog_out, file = "TTU_catalog_section.xml")
 meta$datasets <- meta$datasets[1:4]
 iso_out <- whisker::whisker.render(sb_iso_template, meta)
 cat(iso_out, file = paste0(meta$sciencebase_id, ".xml"))
-
-
-
-
-# meta <- yaml::read_yaml('config_meta.yaml')
-# meta <- meta[!unlist(lapply(meta, is.null))]
-# meta[['lat_size']] <- meta$lat_max - meta$lat_min
-# meta[['lon_size']] <- meta$lon_max - meta$lon_min
-# meta[['datasets']] <- list(
-#   dataset_name = meta$title,
-#   dataset_url_path = 'red_river_2018',
-#   dataset_id = "cida.usgs.gov/red_river_2018",
-#   dataset_meta_location = "metadata/red_river_2018/red_river.ncml",
-#   dataset_title = meta$title,
-#   start_time = meta$start_time,
-#   end_time = meta$end_time,
-#   thredds_url = 'https://cida.usgs.gov/thredds/dodsC/red_river_2018',
-#   service_id = 'OPeNDAP_1')
-# meta[['attribute_description']] <- list(ddx_description = meta$attribute_description[[2]],
-#                                         ddx_url = meta$attribute_description[[1]])
-# 
-# catalog_out <- whisker::whisker.render(catalog_template, meta)
-# cat(catalog_out, file = "red_river_catalog_section.xml")
-# 
-# 
-# 
-# iso_out <- whisker::whisker.render(sb_iso_template, meta)
-# cat(iso_out, file = paste0(meta$sciencebase_id, ".xml"))
